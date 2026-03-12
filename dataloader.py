b0239bee27d4927a2ea212919ac8525055d25af5
Siting Liang <siting.liang@uni-oldenburg.de>
2026-03-12 12:27:16 +0100

    Upload combined labels

--- dataloader.py
+++ dataloader.py
@@ -3,7 +3,7 @@ import torch
 from event_extraction_task_annotation_with_index import get_dataset_instances
 from event_extraction_task_annotation_without_index import get_dataset_instances_no_index, get_dataset_instances_no_index_e2e, get_dataset_instances_no_index_all
 from event_extraction_task_annotation_expressive_prompt import get_dataset_instances_no_index_all as get_dataset_instances_no_index_all_expressive_prompt
-
+from event_extraction_task_annotation_expressive_prompt_combined import get_combined_dataset_instances
 
 class TokenizedEventDataset(torch.utils.data.Dataset):
     """Wraps a list of {'input','output',...} records and tokenizes on initialization.
@@ -167,10 +167,9 @@ def load_e2e_datasets(main_root, ds_keys, split_group, tokenizer, tasks = ['even
             if amount is not None:
                 train_records.extend(split_dict.get('train', [])[:int(len(split_dict.get('train', [])) * amount)])
                 val_records.extend(split_dict.get('val', [])[:int(len(split_dict.get('val', [])) * amount)])
-            
-    train_ds = TokenizedEventDataset(train_records, tokenizer)
-    val_ds = TokenizedEventDataset(val_records, tokenizer)
-    return train_ds, val_ds
+    return train_records, val_records
+     
+   
 
 def load_datasets_all(main_root, ds_keys, split_group, tokenizer, tasks,  use_expressive_prompt=False, splits=None, amount=None):
     
@@ -192,35 +191,73 @@ def load_datasets_all(main_root, ds_keys, split_group, tokenizer, tasks,  use_ex
             if amount is not None:
                 train_records.extend(split_dict.get('train', [])[:int(len(split_dict.get('train', [])) * amount)])
                 val_records.extend(split_dict.get('val', [])[:int(len(split_dict.get('val', [])) * amount)])
-            
+    return train_records, val_records
+
+def tokenize_datasets_all(train_records, val_records, tokenizer):       
     train_ds = TokenizedEventDataset(train_records, tokenizer)
     val_ds = TokenizedEventDataset(val_records, tokenizer)
     return train_ds, val_ds
 
+def load_datasets_all_combined(main_root, split_group, tasks,  amount=None):
+    train_records = []
+    val_records = []
+    
+    
+    instances, stats = get_combined_dataset_instances(main_root,  split_group=split_group)
+    for task_name in tasks:
+            split_dict = instances.get(task_name, {})
+            train_records.extend(split_dict.get('train', []))
+            val_records.extend(split_dict.get('val', []))
+            # Take only percentage of instances instead of full dataset
+            if amount is not None:
+                train_records.extend(split_dict.get('train', [])[:int(len(split_dict.get('train', [])) * amount)])
+                val_records.extend(split_dict.get('val', [])[:int(len(split_dict.get('val', [])) * amount)])
+    return train_records, val_records
 
 
 if __name__ == "__main__":
     # Example usage
     from transformers import AutoTokenizer
+    tasks = {
+        'trigger_identification': {'train': [], 'val': [], 'test': []},
+        'trigger_classification_pipeline': {'train': [], 'val': [], 'test': []},
+        'trigger_classification_e2e': {'train': [], 'val': [], 'test': []},
+        'argument_extraction_pipeline': {'train': [], 'val': [], 'test': []},
+        'argument_identification_e2e': {'train': [], 'val': [], 'test': []},
+        'event_extraction_e2e': {'train': [], 'val': [], 'test': []}
+    }
+    
+    tasks = list(tasks.keys())
 
-    main_root = "/nfs/data/debi5729/processed_data"
+    main_root = "/fs/s6k/groups/agaai/processed_data"
     split_group = "split1"
     datasets = ['geneva']
     tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-small")
-    tasks = ['trigger_identification', 'trigger_classification', 'argument_identification', 'argument_extraction']
-    # check on sample from train by each task
-    instances = get_dataset_instances_no_index(main_root, datasets[0], split_group=split_group, splits=None)
-    for task_name in tasks:
-            split_dict = instances.get(task_name, {})
-            train_records = split_dict.get('train', [])
-            print(task_name)
-            print(train_records[0])
-    # check e2e
-    instances = get_dataset_instances_no_index_e2e(main_root, datasets[0], split_group=split_group, splits=None)
-    split_dict = instances.get('event_extraction_e2e', {})
-    train_records = split_dict.get('train', [])
-    print('event_extraction_e2e')
-    print(train_records[0])
+    # check the instances of records
+    datasets = ['geneva',"caise", "genia2013"]
+    train_records, val_records = load_datasets_all(main_root, datasets, split_group, tokenizer, tasks, use_expressive_prompt=False)
+    print(len(train_records), len(val_records))
+    for i in [10,  5000]:
+        print("Example train record {}:".format(i), train_records[i])
+    
+    for i in [10,  500]:
+        print("Example val record {}:".format(i), val_records[i])
+        
+    combined_train_records, combined_val_records = load_datasets_all_combined(main_root, split_group, tasks, amount=1)
+    print(len(combined_train_records), len(combined_val_records))
+    
+    for i in [10,  5000]:
+        print("Example combined train record {}:".format(i), combined_train_records[i])
+    for i in [10,  500]:
+        print("Example combined val record {}:".format(i), combined_val_records[i])
+    
+    train_records = train_records + combined_train_records
+    val_records = val_records + combined_val_records
+    train_ds, val_ds = tokenize_datasets_all(train_records, val_records, tokenizer)
+    print("Number of tokenized train examples:", len(train_ds))
+    print("Number of tokenized val examples:", len(val_ds)) 
+    
+    
     
     
    
